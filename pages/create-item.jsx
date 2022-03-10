edbd696e48c623c8a0d77f4369500d76ed81602d
John Murphy <jmurphy5613@gmail.com>
2022-03-10 17:50:07 -0500

    sync

--- pages/create-item.jsx
+++ pages/create-item.jsx
@@ -4,8 +4,10 @@ import { create as ipfsHttpClient } from "ipfs-http-client";
 import { useRouter } from "next/router";
 import { Web3Modal } from "web3modal";
 
-import { Input, Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 
 import Navbar from '../components/navbar';
 
@@ -20,16 +22,20 @@ import NFTMarket from "../artifacts/contracts/market.sol/NFTMarket.json";
 const useStyles = makeStyles((theme) => ({
     root: {
         backgroundColor: theme.palette.secondary.main,
-        minHeight: "100vh",
+        minHeight: '100vh',
+        minWidth: '100vw',
         margin: 0,
         padding: 0,
-        display: 'flex',
-        justifyContent: 'center',
-    },
+    },  
     mainTitle: {
         color: '#ffffff',
         fontFamily: theme.typography.fontFamily.primary,
-        margin: '3rem'
+    },
+    mainContent: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '7vh',
     }
 }));
 
@@ -37,7 +43,7 @@ const useStyles = makeStyles((theme) => ({
 const CreateItem = () => {
 
     const classes = useStyles();
-    
+
     const [fileUrl, setFileUrl] = useState();
     const [formInput, setFormInput] = useState({ name: "", description: "", price: "" });
     const router = useRouter();
@@ -45,14 +51,17 @@ const CreateItem = () => {
 
 
     return (
-        <>
-            <Navbar />
             <div className={classes.root}>
-                <Typography variant="h2" className={classes.mainTitle}>
-                    Upload Your Art
-                </Typography>
+                <Navbar />
+                <Container maxWidth="md" className={classes.mainContent}>
+                    <Typography variant="h2" className={classes.mainTitle}>
+                        Upload Your Art
+                    </Typography>
+                    <Button variant="" style={{ width: '100%' }}>
+                        Submit
+                    </Button>
+                </Container>
             </div>
-        </>
 
     )
 }
