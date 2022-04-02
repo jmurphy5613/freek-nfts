d592d4c2e6a8c0a8ceabaa85bf5dc34d502fee51
John Murphy <jmurphy5613@gmail.com>
2022-04-01 21:56:54 -0400

    set up nft contract

--- contracts/nft.sol
+++ contracts/nft.sol
@@ -1,28 +1,24 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 pragma solidity ^0.8.3;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-import "hardhat/console.sol";
 
 contract NFT is ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-    address contractAddress;
 
-    constructor(address marketplaceAddress) ERC721("Metaverse", "METT") {
-        contractAddress = marketplaceAddress;
+    uint public itemCounter = 0;
+
+    constructor() ERC721("Freek NFT", "FRK") {}
+
+    function mint(string memory _tokenURI) external returns(uint) {
+        itemCounter++;
+
+        _safeMint(msg.sender, itemCounter);
+        _setTokenURI(itemCounter, _tokenURI);
+        
+        return itemCounter;
     }
 
-    function createToken(string memory tokenURI) public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
 
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        setApprovalForAll(contractAddress, true);
-        return newItemId;
-    }
 }
